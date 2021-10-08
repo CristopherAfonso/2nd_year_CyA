bbe5c2e087997b4220547c369cb4a055b47ad398
CristopherAfonso <alu0101402031@ull.edu.es>
2021-10-08 17:08:29 +0000

    Archivos del programa creados, pero solo he empezado el makefile

---
+++ p02_strings/main_strings.cc
@@ -0,0 +1,22 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingenieria y Tecnologia
+// Grado en Ingenieria Informatica
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Practica 2: Simbolos, alfabetos y cadenas
+// Autor: Cristopher Manuel Afonso Mora
+// Correo: alu0101402031@ull.es
+// Fecha: 08/10/2021
+// Archivo main_strings.cc: programa cliente.
+//         Contiene la funcion main del proyecto que usa las clases X e Y que
+//         realizan el trabajo de leer el fichero de entrada identificando 
+//         patrones en el texto que dan lugar al fichero de salida.
+//
+// Referencias:
+//         Enlaces de interes
+//
+// Historial de revisiones
+//         08/10/2021 - Creacion (primera version) del codigo
+
+#include <iostream>
+#include <fstream>
